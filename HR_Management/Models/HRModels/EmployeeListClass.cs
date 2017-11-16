88797209f8ef0793c1ac5d2cf6a35686e8694f12
Eduardo Jean <eduardojean9@gmail.com>
2017-11-16 09:57:16 -0500

    Added Class for Employee Controller

---
+++ HR_Management/Models/HRModels/EmployeeListClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Management.Models.HRModels
+{
+    public class EmployeeListClass
+    {
+       public string fname { get; set; }
+       public string lname { get; set; } 
+       public string position { get; set; }
+       public string status { get; set; }
+       public string email { get; set; }
+       public int rank { get; set; }
+       public int managerID { get; set; }
+       
+    }
+}
