2822686fa8bf1aaae9d91d1743808ea5164f75f7
Eduardo Jean <eduardojean9@gmail.com>
2017-11-22 18:50:54 -0500

    Complaints Page Updated

--- HR_Management/Controllers/ComplaintsController.cs
+++ HR_Management/Controllers/ComplaintsController.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using HR_Management.Models;
+using HR_Management.Models.HRModels;
+using HR_Management.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HR_Management.Controllers
 {
 	public class ComplaintsController : Controller
 	{
-		public IActionResult ViewComplaint()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public ComplaintsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+            _signInManager = signInManager;
+        }
+        public IActionResult ViewComplaint()
 		{
 			ViewData["Message"] = "Page to view a complaints.";
 
@@ -22,25 +36,45 @@ namespace HR_Management.Controllers
 			return View();
 		}
 
-		public IActionResult ComplaintsIndex()
+		public async Task<IActionResult> ComplaintsIndex()
 		{
+            //Lists regular employee complaints
 			ViewData["Message"] = "Page to view all the complaints.";
-
-			return View();
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return View("Error");
+            }
+            Employee emp = _context.Employee.Where(x => x.appuserid == user.Id).First();
+            var CurrentComplaintList = _context.Complaints.Where(x => x.empId == emp.empId);
+            ViewData["CurrentEmployeeComplaints"] = new SelectList(CurrentComplaintList);
+            return View();
 		}
 
-		public IActionResult ComplaintsIndexManager()
+		public async Task<ActionResult> ComplaintsIndexManager()
 		{
+            //Lists complaints for manager
 			ViewData["Message"] = "Page to view all the complaints manager view.";
-
-			return View();
+            //Lists regular employee complaints
+            ViewData["Message"] = "Page to view all the complaints.";
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return View("Error");
+            }
+            Employee emp = _context.Employee.Where(x => x.appuserid == user.Id).First();
+            var CurrentComplaintList = _context.Complaints.Where(x => x.empId == emp.empId);
+            ViewData["CurrentManagerComplaints"] = new SelectList(CurrentComplaintList);
+            return View();
 		}
 
 		public IActionResult OutstandingComplaints()
 		{
+            //If logged in user is HR, lists all company complaints
 			ViewData["Message"] = "Page to view all the team complaints.";
-
-			return View();
+            var AllCompanyComplaints = _context.Complaints;
+            ViewData["AllComplaints"] = new SelectList(AllCompanyComplaints);
+            return View();
 		}
 
 		public IActionResult EditComplaintStatus()
@@ -68,5 +102,9 @@ namespace HR_Management.Controllers
 		{
 			return View();
 		}
-	}
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+    }
 }
